b64e63d525c014413afb98b5a4a125c02d379fcd
unknown <kaja.ricna@seznam.cz>
2021-04-11 16:43:42 +0200

    Styly

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#stihacky").hide();
+    $(" h2").on("click", function(){
+        $(this).nextUntil("h2").toggle(1000);
+    });
+    $("h3:even").append().css({"color" : "blue"});
+    $("#susButton").on("click", ()=>{
+        $("main").hide();
+        $("footer").hide();
+        $("nav").hide();
+        $("p").hide();
+        $("header").removeClass("jumbotron-fluid bg-secondary");
+        $("h1").text("Letí dvě stíhačky a jedna nestíhá");
+        $("#stihacky").show();
+        
+    });
+});
