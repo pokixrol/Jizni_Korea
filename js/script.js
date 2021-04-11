2359be994ad8f97a8298f73dfb4293780ca84d98
unknown <kaja.ricna@seznam.cz>
2021-04-11 17:30:15 +0200

    Odkazy + styly

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(function(){
     $(" h2").on("click", function(){
         $(this).nextUntil("h2").toggle(1000);
     });
-    $("h3:even").append().css({"color" : "blue"});
+    //$("h3:even").append().css({"color" : "blue"});
     $("#susButton").on("click", ()=>{
         $("main").hide();
         $("footer").hide();
         $("nav").hide();
-        $("p").hide();
+        $("p").text("ha ha");
         $("header").removeClass("jumbotron-fluid bg-secondary");
         $("h1").text("Letí dvě stíhačky a jedna nestíhá");
         $("#stihacky").show();
