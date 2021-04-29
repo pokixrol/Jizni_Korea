003aaef26a63476c777f7a4a19bab8755f838501
unknown <kaja.ricna@seznam.cz>
2021-04-29 17:10:26 +0200

    historické osobnosti + styly

--- js/historie.js
+++ js/historie.js
@@ -81,6 +81,137 @@ const events = [
       },
 ];
 
+const heroes = [
+   {
+      "name":"Tchädžo (Korjo)",
+      "birth":"31. ledna 877 Kesong",
+      "cross":"fas fa-cross",
+      "death":"4. července 943 Kesong",
+      "biography":"Tchädžo (태조), narozený s jménem Wang Kŏn ( 왕건), byl zakladatelem dynastie Korjo vládnoucím stejnojmennému království v letech 918–943. Narodil se v Songaku (송악) do kupecké rodiny obchodující na Rjesŏnggangu v tehdejším království Sjednocená Silla. V počátcích kariéry Wang Kŏna, které spadají do období pozdních třech království, se Silla nacházela v obtížné situaci, kdy královna Činsŏng postrádala autoritu a řada místních vůdců ji neuznávala. Na jihovýchodě mezi nimi vynikal Kjŏn Hwŏn, kterému se začalo dařit povstalce sjednocovat. V roce 895 dorazil Kung Je se svým vojskem do Songaku a Wang Kŏn i jeho otec se podřídili jeho autoritě. Wang Kŏn se stal součástí jeho vojska a brzy se stal pro Kung Je jeho oblíbeným generálem. V roce 900 vedl v oblasti Čchungdžu úspěšné tažení proti vojsku Pozdního Päkče. V roce 903 pokračovalo úspěšné námořní tažení u Nadžu. V roce 913 se stal předsedou vlády nově vzniklého Tchäbongu. Kung následně ztratil podporu, když se začal prohlašovat za Buddhu a tvrdě pronásledovat ty, kdo s jeho pojetím náboženství nesouhlasili. Kromě mnoha mnichů nechal popravit i vlastní manželku s dvěma syny. V roce 918 se proto dohodli čtyři generálové, Hong Ju, Pä Hjŏn-gjŏng, Sin Sung-gjŏm a Pok Či-gjŏm, že Kunga svrhnou a králem zvolí Wanga Kŏna. Kung je byl následně zabit v oblasti Čchorwonu, kde měl své hlavní město, a králem zvolen Wang Kŏn. Království přejmenoval na Korjo a přesunul hlavní město do Kägjongu. V roce 927 Kjŏn Hwŏn dobyl Kjŏngdžu, hlavní město Silly, zabil krále, dosadil svého loutkového vládce a táhl se svým vojskem proti Korju. Tchädžo proti němu rovněž vytáhl. V bitvě sice utrpěl drtivou porážku, ale království se po ní rychle zotavilo a v pozdějších obranných střetnutích už vítězilo. V roce 935 dospěl král Silly Kjŏngsun k poznání, že zbytek jeho království je neudržitelný, a předal Tchädžovi vládu nad Sillou, který ho nad ní obratem učinil správcem. V roce 936 se Tchädžovi podařilo dobýt Pozdní Päkče a sjednotil tím Koreu podobně, jako ji na vrcholu své moci sjednotila Sjednocená Silla. Tchädžo zemřel následkem nemoci v 4. července 943 ve věku 66 let a na trůn po něm nastoupil jeho syn Hjedžong.",
+      "portraits":[
+         "",
+      ],
+      "url":"https://cs.wikipedia.org/wiki/Tch%C3%A4d%C5%BEo_(Korjo)",
+      "class":"",
+   },
+   {
+      "name":"Tchädžo (Čoson)",
+      "birth":"11. října 1335 Hamhung",
+      "cross":"fas fa-cross",
+      "death":"30. března 1450 Soul",
+      "biography":"Tchädžo (태조), narozen jako I Song-gje (이성계) a přejmenován I Dan (이단), byl zakladatel a první král korejského státu Čoson, vládl v letech 1392 až 1398.  I Song-gje pocházel ze severovýchodní Koreje. Korea byla do poloviny 50. let 14. století součástí mongolské říše Jüan a předkové I Song-gjea zaujímali vysoké posty v jüanské administrativě. Jeho otec I Dža-čchun byl generálem korejské armády, I Song-ge následoval jeho příkladu. Získal popularitu v úspěšných bojích s Mongoly, japonskými piráty wo-kchou útočícími na pobřeží, porazil armádu čínských protimongolských povstalců – rudých turbanů, která vtrhla do Koreje. Během 80. let se korejský dvůr rozdělil na dvě strany – skupinu v čele s generálem I Song-gjem, orientovanou na podporu čínské říše Ming, která vyhnala z Číny jüanské armády, a skupinu v čele s generálem Čchö Jongem, která dávala přednost podpoře jüanských Mongolů. Spory I Song-gje vyřešil obrácením armády (která měla za úkol invazi do mingského poloostrova Liao-tung) proti hlavnímu městu, kde provedl státní převrat, odstranil opozici a dosadil nového krále Čchanga. Už následující rok ho vyměnil za krále Kongjanga, kterého ponechal na trůnu tři roky. Roku 1392 krále sesadil a sám nastoupil na trůn, přitom přejmenoval stát z Korjo na Čoson („Země jitřní svěžesti“). Po nástupu na trůn navázal a udržoval diplomatické styky s okolními i vzdálenějšími státy. Podporoval buddhismus, reorganizoval armádu a  provedl zemědělskou reformu. Roku 1392 jmenoval následníkem svého osmého syna I Pang-seoka. Roku 1398 však jeho pátý syn I Pang-won provedl státní převrat, zabil prvního ministra Čong Do-čona a přiměl otce k abdikaci. Trůn zprvu předal svému staršímu bratru I Pang-gwaovi, roku 1400 se králem stál sám. I Song-gje zemřel roku 1408, znám je pod svým posmrtným jménem jako král Tchädžo.",
+      "portraits":[
+         "",
+      ],
+      "url":"https://cs.wikipedia.org/wiki/Tch%C3%A4d%C5%BEo_(%C4%8Coson)",
+      "class":"",
+   },
+   {
+      "name":"Sedžong Veliký",
+      "birth":"7. května 1397 Soul",
+      "cross":"fas fa-cross",
+      "death":"24. května 1408 Hansongbu",
+      "biography":"Sedžong Veliký byl čtvrtý panovník korejského království Čoson, kterému vládl v letech 1418 až 1450. Narodil se jako třetí syn krále z dynastie Čoson Tchedžonga a královny Min. Za jeho vlády nastal kulturní a hospodářský rozkvět. Prosazoval konfuciánskou politiku a výrazně omezil moc buddhistických kněží. Na jeho popud bylo v roce 1446 vytvořeno korejské hláskové písmo hangul, kterému však trvalo 400 let, než nahradilo původní složité čínské znaky. Je jedním ze dvou panovníků v korejských dějinách, kteří si vysloužili přízvisko Veliký.",
+      "portraits":[
+         "sendzong_veliky.jfif",
+         "sendzong_veliky2.jpg",
+      ],
+      "url":"https://cs.wikipedia.org/wiki/Sed%C5%BEong_Velik%C3%BD",
+      "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+   },
+   {
+      "name":"I Sun-sin",
+      "birth":"28. dubna 1545 Soul",
+      "cross":"fas fa-cross",
+      "death":"16. prosince 1598 Namhä",
+      "biography":"I Sun-sin (이순신) byl korejský admirál. V letech 1591-1598 během Imdžinské války vyhrál pro dynastii Čoson řadu významných námořních bitev.mPřestože neměl žádné formální vojenské vzdělání a jeho námořnictvo bylo nepočetné a špatně vybavené, jako jeden z mála námořních vojevůdců ve světové historii nebyl nikdy poražen v žádné bitvě, ačkoli jich vybojoval nejméně 23. Za jeho největší úspěch je považována bitva u Mjongnjangu v roce 1597, kdy jeho loďstvo čítající pouhých 13 lodí na hlavu porazilo 333 lodí japonského námořnictva. Tato událost byla předlohou filmu Admirál: Bouřlivé proudy (1) z roku 2014. I zemřel v bitvě u Norjangu v roce 1598. Je považován za jednoho z největších Korejců a někteří historici ho považují za korejského Horatia Nelsona",
+      "portraits":[
+         "i_sun_sin.jpg",
+         "i_sun_sin2.jpg",
+      ],
+      "url":"https://cs.wikipedia.org/wiki/I_Sun-sin",
+      "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+   },
+   {
+      "name":"I Sung-man",
+      "birth":"26. března 1875 Hedžu, Korejské království",
+      "cross":"fas fa-cross",
+      "death":"19. července 1965 Honolulu, Havaj, Spojené státy americké",
+      "biography":"Absolvoval chlapeckou misionářskou školu vedenou metodisty, roku 1898 byl uvězněn za podporu Klubu nezávislosti, studoval v USA (Univerzita George Washingtona), kde organizoval odboj proti japonské okupaci – doktorát získal roku 1910 v Princetonu. V roce 1919 byl prezidentem „Prozatímní šanghajské vlády“ a exilovou hlavou státu zůstal až do roku 1939. Vůdcem nově vzniklé Jižní Koreje se stal v roce 1948 a během svého funkčního období byl konfrontován s korejskou válkou, kterou vyvolal komunistický sever v čele s Kim Ir-senem. Ve vykonávání svého úřadu měl spíše nacionalistické diktátorské tendence, nejvíce se podobal posledním králům dynastie Čoson. V roce 1956 nechal změnit ústavu, aby mohl být potřetí zvolen prezidentem. Po studentských nepokojích v roce 1960 byl nucen odejít do vyhnanství. Bývá mu připisována vražda jednoho z nejvýznamnějších jihokorejských politiků Kima Ku.",
+      "portraits":[
+         "i_sung_man.jpg",
+         "i_sung_man2.jpg",
+      ],
+      "url":"https://cs.wikipedia.org/wiki/I_Sung-man",
+      "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+   },
+ {
+   "name":"Jon Po-son",
+   "birth":"26. srpna 1897 Asan, Korejské království",
+   "cross":"fas fa-cross",
+   "death":"18. července 1990 Soul, Jižní Korea",
+   "biography":"Jon Po-son byl jihokorejský politik. V letech 1960–1962 byl prezidentem Jižní Koreje. V letech 1949–1950 byl ministr obchodu a průmyslu. Zastával též funkce starosty Soulu či prezidenta jihokorejského Červeného kříže. Zpočátku stál na straně poválečného autoritářského prezidenta I Sung-mana, který ho jmenoval do vlády i do pozice starosty Soulu. Postupně však Jon stále více kritizoval nedemokratické praktiky a dostával se do opozice. Když byl I Sung-man roku 1960 přinucen rezignovat po velkých studentských bouřích, byl Jon Po-son zvolen prezidentem, jako figura, která symbolizovala demokratizační snahy, a která může v zemi zavést skutečný parlamentní systém. Jon se o to skutečně pokoušel, ale čelil stále většímu chaosu v zemi. Nedařilo se mu ani poskládat skutečně funkční kabinet. Roku 1961 proto provedl generál Pak Čong-hui vojenský převrat a převzal moc v zemi. Jon zůstal nějaký čas ve funkci a pokoušel se s Pakem spolupracovat, ale nakonec pochopil, že Pak míří k diktatuře a roku 1962 rezignoval. Byl poté jedním z hlavních kritiků dlouhé Pakovy vlády.",
+   "portraits":[
+      "jon_po_son.jpg",
+      "jon_po_son2.jpg",
+   ],
+   "url":"https://cs.wikipedia.org/wiki/Jon_Po-son",
+   "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+},
+{
+   "name":"Pak Čong-hui",
+   "birth":"30. září 1917 Kumi, Korea",
+   "cross":"fas fa-cross",
+   "death":"26. října 1979 Soul, Jižní Korea",
+   "biography":"Pak Čong-hui (박정희) byl jihokorejský voják a politik. V letech 1961-1979 byl diktátorem a v letech 1963-1979 oficiálně prezidentem Jižní Koreje. Během své vlády učinil jihokorejskou ekonomiku jednou z nejsilnějších na světě. Roku 1999 ho časopis Time zařadil mezi 10 nejdůležitějších Asiatů 20. století.  Po pádu prvního jihokorejského prezidenta I Sung-mana se země zmítala v chaosu. V této atmosféře provedl Pak Čong-hui vojenský puč. Základem jeho vlády se stala nově zřízená tajná policie (KCIA), která měla pravomoc zatýkat nepřátele vlády a režimu, běžné bylo i mučení. Na přání americké administrativy pak nicméně formálně zcivilnil svou vládu a stal se roku 1963 prezidentem. Původně slíbil, že odejde z funkce roku 1971, ale slib nedodržel. Pak byl zvolen třikrát při přímých prezidentských volbách na post prezidenta, pokaždé s více než 80% hlasy. Po roce 1972 další přímá volba však již nebyla možná, proto volby kontroloval až do doby, kdy byl zavražděn v roce 1979. Zatímco v počátcích vlády měl Pak ve veřejnosti spíše podporu, po roce 1971 ji rychle ztrácel. Nespokojenost vyvrcholila mohutnými demonstracemi roku 1979, které organizovaly především studenti. Na 400 lidí bylo zatčeno, nepokoje však neustávaly, demonstranti začali útočit na policejní stanice i sídla vládnoucí strany. Během vrcholných nepokojů byl Pak zavražděn šéfem tajné služby Kim Čä-gjuem. Ten se hájil vlastenectvím a tím, že Pak byl překážkou na cestě k demokracii. Průběh atentátu a jeho motivy jsou ovšem dodnes nejasné. Pakova zahraniční politika stála, jako u jeho předchůdců, na vztazích se Spojenými státy. S tím souviselo o jihokorejské angažmá ve vietnamské válce, Pak poslal do Vietnamu 320 tisíc vojáků. Jeho dcera Pak Kun-hje byla v letech 2013–2017 prezidentkou Jižní Koreje. Pakův hlavní přínos byl v ekonomické oblasti. Rozhodl se vybudovat silně exportní a vládou přísně řízenou ekonomiku. Infrastrukturu financoval především z japonských investic. Po Korejské válce se Jižní Korea nacházela ve velké chudobě. Pak se rozhodl zaměřit na těžký a chemický průmysl. Jediné, co potřeboval, byla silná pracovní síla. Korejský lid byl odhodlaný pracovat s vidinou lepšího života pro jejich děti.",
+   "portraits":[
+      "pak_cong_hui.png",
+      "pak_cong_hui2.jpg",
+   ],
+   "url":"https://cs.wikipedia.org/wiki/Pak_%C4%8Cong-hui",
+   "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+},
+{
+   "name":"Čon Tu-hwan",
+   "birth":"18. ledna 1931 Hapcheon-gun, Korea",
+   "cross":"",
+   "death":"",
+   "biography":"Čon Du-hwan je jihokorejský voják a politik. V letech 1951–1955 vystudoval Korejskou vojenskou akademii. Mnoho jeho tehdejších spolužáků mu později pomohlo získat moc ve vojenském převratu. V roce 1961 silně podporoval Pak Čong-hui, který se ujal moci vojenským pučem a začal zemi vládnout jako diktátor. To mu zajistilo rychlý vzestup a práci v Pakově blízkosti. Roku 1963 začal pracovat v tajné službě jako personální ředitel. V letech 1970–1971 bojoval ve vietnamské válce. Roku 1979, během velkého neklidu v zemi, byl Pak Čong-hui zavražděn šéfem tajné služby. Čon byl pověřen vyšetřením atentátu, jasno do případu však nikdy vneseno nebylo, což podnítilo spekulace o tom, že Čon byl do atentátu zapleten. Na Pakovo místo nastoupil  Čchö Kju-ha, který v prosinci 1979 vyhrál prezidentské volby. Čon proti němu však velmi brzy se skupinou armádních velitelů začal konspirovat. Postupnými kroky získával stále větší vliv. Čchö se pokusil Čonovu touhu po moci ukojit tím, že ho v dubnu 1980 jmenoval šéfem tajné služby. Již v květnu však Čon vyhlásil stanné právo, oficiálně zdůvodněné údajným proniknutím severokorejských agentů do země, a převzal na sebe všechny exekutivní pravomoci. Vypuklé studentské protesty brutálně potlačil, proslulým je zejména masakr v Kwangdžu, kde vojáci pozabíjeli 987 demonstrantů. Čchö poté rezignoval a Čon se nechal v září 1980 zvolit prezidentem. Čon slíbil, že úřad prezidenta opustí po sedmi letech a Korejci očekávali, že po tomto období nastanou demokratizační změny. Roku 1987 však Čon představil svého nástupce, Ro Te-ua, a oznámil, že by měl být zvolen nepřímými volbami. To spustilo vlnu protestů, zejména studentů. Čon si nějaký čas udržoval ještě určitý vliv, ale pozvolna v jihokorejské společnosti padalo tabu zločinů jeho diktatury, zejména z roku 1979. Roku 1996 byl Čon nakonec postaven před soud a odsouzen k smrti, především za masakr v Kwangdžu. Dostal ovšem milost od prezidenta Kim Jong-sama.",
+   "portraits":[
+      "con_tu_hwan.jpg",
+      "con_tu_hwan2.jpg",
+   ],
+   "url":"https://cs.wikipedia.org/wiki/Pak_%C4%8Cong-hui",
+   "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+},
+{
+   "name":"Ro Tche-u",
+   "birth":"4. prosince 1932 Tegu, Korea",
+   "cross":"",
+   "death":"",
+   "biography":"Ro Tche-u je jihokorejský voják a politik. V letech 1988–1993 byl prezidentem Jižní Koreje. Byl jedním z hlavních organizátorů letních olympijských her v Soulu roku 1988. Jeho předchůdce, autoritativní prezident Čon Du-hwan, ho na začátku osmdesátých let povolal z armády do vlády, kde zastával různé pozice, stal se především Čonovou pravou rukou. Byl pověřen i organizováním olympijských her. Čon ho označil jako svého nástupce roku 1987, což vyvolalo vlnu protestů, neboť zejména demokraticky naladění studenti očekávali od Čonova odchodu uvolnění režimu, zatímco volba Roa pro ně znamenala de facto prodloužení Čonovy vlády. Ro se pokusil hnutí odporu uklidnit představením rozsáhlého reformního programu a slibem, že prezidentské volby budou skutečně svobodné. To také splnil a je tudíž označován za prvního skutečně demokraticky zvoleného jihokorejského prezidenta. Kim Jong-sam obvinil roku 1993 Roa z korupce a sám stanul v čele země. Oba však byli později souzeni za podíl na masakru v Kwangdžu, který se odehrál roku 1979, v počátcích vlády Čona.",
+   "portraits":[
+      "ro_tche_u.jpg",
+      "ro_tche_u2.jpg",
+   ],
+   "url":"https://cs.wikipedia.org/wiki/Ro_Tche-u",
+   "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+},
+{
+   "name":"Kim Te-džung",
+   "birth":"6. leden 1924 Haŭi, Korea",
+   "cross":"fas fa-cross",
+   "death":"18. srpna 2009 Soul, Jižní Korea",
+   "biography":"Kim Te-džung (김대중) byl 8. prezidentem Korejské republiky v letech 1998 až 2003 a jihokorejským politikem, který 40 let významně ovlivňoval místní politickou scénu, byl členem a zakladatelem řady politických stran, držitel Nobelovy ceny za mír za rok 2000. Kim Te-džung se narodil v období japonské koloniální správy, podle oficiálních životopisů na ostrově Haŭi nedaleko přístavního města Mokpcho na jihozápadě poloostrova. Ambiciózní Kim o vstup do politiky usiloval již od konce korejské války, ale poslancem Národního shromáždění za opoziční Demokratickou stranu byl poprvé zvolen až v roce 1961. V důsledku vojenského převratu však byl parlament rozpuštěn a Kim mohl v poslanecké lavici zasednout až po volbách v roce 1963. V průběhu 60. let se Kim Te-džung spolu s Kim Jŏng-samem stal jedním z významnějších „mladších“ představitelů opoziční Demokratické strany. V hlasování Nové demokratické strany o stranickém kandidátovi na funkci prezidenta Kim Te-džung těsně zvítězil nad Kim Jŏng-samem.  Kim Te-džung v roce 1972 uchýlil do Japonska. Existují tvrzení o tom, že mu jihokorejská vláda dokonce opakovaně usilovala o život. V lednu 1973 jej agenti jihokorejské tajné služby unesli zpět do Koreje. Na jaře 1976 byl odsouzen k osmi letům odnětí svobody, o rok později soud trest snížil na pět let a v prosinci 1978 byl odeslán do domácího vězení. Po své inauguraci v únoru 1998 prezident Kim Te-džung vyhlásil tzv. „občanskou vládu“ a zahájil Program slunečního paprsku, který mu prospěl při zisku Nobelovy ceny za mír pro rok 2000 a nezanedbatelně rovněž prospěl severokorejskému režimu přísunem trvale chybějících financí. Kim Te-džung zemřel 18. srpna 2009 v Soulu na následky infarktu.",
+   "portraits":[
+      "kim_te_jung.jpg",
+      "kim_te_jung2.jpg",
+   ],
+   "url":"https://cs.wikipedia.org/wiki/Kim_Te-d%C5%BEung",
+   "class":"col-lg-6 col-md-6 col-sm-12 text-center",
+},
+];
+
 $(function(){
 
     $("h2").on("click", function(){
@@ -119,4 +250,30 @@ $(function(){
         $(".event-text").hide();
         $(this).parent().next().show(500);
     });  
+    heroes.forEach((hero)=>{
+      $("#postavy .list-group").append(`<li class="list-group-item list-group-item-action list-group-item-primary">${hero.name}</li>`);
+  });
+
+  function fillPersonCard(person) {
+      let hero = heroes.find(item => {return item.name === person});
+      $(".card-header").html(`<i class="fas fa-star-of-life"></i> <b>${hero.birth}</b> <i class="${hero.cross}"></i> <b>${hero.death}</b>`);
+      $(".card-title").text(hero.name);
+      $(".card-text").text(hero.biography);
+      $(".card-footer").html(`Pro více informací klikněte <a href="${hero.url}" target="_blank"><b>ZDE</b></a>.`);
+      $(".gallery").empty();
+      for (let i = 0; i < hero.portraits.length; i++) {
+          $(".gallery").append(`<div class="${hero.class}"><img src="../img/${hero.portraits[i]}" alt="" class="img-fluid"></div>`);        
+      }
+  }
+  $("#postavy li:first").addClass('active');
+  fillPersonCard(heroes[0].name);
+  $("#postavy li").on("click", function(){
+      $("#postavy li").removeClass("active");
+      $(this).addClass("active");        
+      let person = $(this).text();
+      $("#portret").slideUp(1000, function(){
+          fillPersonCard(person);
+      });
+      $("#portret").slideDown(1000);
+  });
 })
