f0a8d2132497423e9a5b0143026728290549691b
unknown <kaja.ricna@seznam.cz>
2021-06-15 20:01:50 +0200

    Geografie update

--- public/js/geografie.js
+++ public/js/geografie.js
@@ -2,6 +2,10 @@ $(function(){
     $("h2").on("click", function(){
         $(this).parents(".row").next().toggle(1000);
     });
-
+    $("#reky").hide();
+    $("#river").on("click", function(){
+        $("#idk").hide(500);
+        $("#reky").show(1000);
+    });
 
 })
--- public/js/mapa.js
+++ public/js/mapa.js
@@ -1,41 +1,93 @@
-const towns =[
-    {
-       "id":"",
-       "city":"",
-       "peoples":11111,
-       "sign":"",
-       "text":""
-    },
-    {
-       "id":"",
-       "city":"",
-       "peoples":11111,
-       "sign":"",
-       "text":""
+$(function () {
+  let fillProvincie = $(".provincie").css("fill");
+  $(".provincie").on("mouseover", function () {
+    if ($("#kraje:checked").val()) {
+      $(".provincie").css("fill", fillProvincie);
+      $(this).css("fill", "rgb(200, 16, 46)");
+      $(this).popover({
+        trigger: "click",
+        placement: "right",
+        offset: "-10, -100",
+        title: $(this).find("title").text(),
+      });
     }
- ]
-
-
-$(function(){
-     let fillProvincie = $('.provincie').css('fill');
-    $('.provincie').on('mouseover', function(){
-        $('.provincie').css('fill', fillProvincie);
-     $(this).css('fill','rgb(200, 16, 46)');
+  });
+  $(".provincie").on("mouseout", function () {
+    $(this).css("fill", fillProvincie);
+  });
+  function townsBlock(towns) {
+    let fillMesto = $("rect, .specialni_mesto").css("fill");
+    $("rect, .specialni_mesto").on("click", function () {
+      $("rect, .specialni_mesto").css("fill", fillMesto);
+      $(this).css("fill", "rgb(51, 204, 255)");
+      console.log($(this).children("title").text());
+      let id = $(this).attr("id");
+      let mesto = towns.find((item) => {
+        return item.id == id;
+      });
+      console.log(mesto);
+      $("#info").fadeOut(1000, function () {
+        $("#info").html(
+          `<div class="col-12"><h2>${
+            mesto.city
+          } <small class="text-small">${new Intl.NumberFormat("cs-CS").format(
+            mesto.peoples
+          )} obyvatel</small></h2></div><div class="col-2"><img src="../geografie/images/${
+            mesto.img
+          }" class="img-fluid"></div><div class="col-10">${mesto.text}</div>`
+        );
+      });
+      $("#info").fadeIn(1000);
     });
-    
-    let fillMesto = $('.specialni_mesto').css('fill');
-    $('.specialni_mesto').on('mouseover', function(){
-        $('.specialni_mesto').css('fill', fillMesto);
-     $(this).css('fill','rgb(0, 47, 108)');
+  }
+
+  fetch("../geografie/data/towns.json")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      townsBlock(json);
+    })
+    .catch(function (error) {
+      console.error("Chyba: \n", error);
     });
- 
-    let fillRect = $('rect').css('fill');
-    $('rect').on('click', function(){
-       $('rect').css('fill', fillRect);
-     $(this).css('fill','yellow');
-    console.log($(this).children('title').text());
-    let id = $(this).attr('id');
-     let mesto = towns.find(function(obj) { return obj.id == id});
-     console.log(mesto);
- });
- })
+  function unescoBlock(unesco) {
+    $("circle").hide();
+    $("#mesta").on("change", function () {
+      $("rect").toggle(500);
+      $(".specialni_mesto").css("fill", fillProvincie);
+    });
+    $("#unesco").on("change", function () {
+      $("circle").toggle(500);
+    });
+    let fillUnesco = $("circle").css("fill");
+    $("circle").on("click", function () {
+      $("circle").css("fill", fillUnesco);
+      let id = $(this).attr("id");
+      $(this).css("fill", "rgb(51, 204, 255)");
+      let pamatka = unesco.find((item) => {
+        return item.id == id;
+      });
+      $("#info").slideUp(1000, function () {
+        $("#info").html(
+          `<div class="col-12"><h2>${pamatka.name} <small class="text-small">(od roku ${pamatka.year})</small></h2></div><div class="col-4"><img src="../geografie/images/${pamatka.photo}" class="img-fluid"></div><div class="col-8"><p>${pamatka.description}</p><p><a href="${pamatka.url}" target="_blank" class="btn btn-info">Podrobnosti</a></p></div>`
+        );
+      });
+      $("#info").slideDown(1000);
+    });
+  }
+  fetch("../geografie/data/unesco.json")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      unescoBlock(json);
+    })
+    .catch(function (error) {
+      console.error("Chyba: \n", error);
+    });
+});
